cdd9b8f3859562e55caf9fb64f606438e440f69b
Tori Wood <victoria.ashley.wood@gmail.com>
2015-11-19 22:13:50 -0500

    Update todo items validations

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -22,6 +22,28 @@ let!(:todo_list) {TodoList.create(title: "Grocery list", description: "Groceries
 		end
 	end
 
+	it "displays an error with no content" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
 
+		within("div.flash") do
+		expect(page).to have_content("There was a problem adding that todo list item.")
+		end
+		expect(page).to have_content("Content can't be blank")
+	end
+
+	it "displays an error with content less than 2 characters" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: "1"
+		click_button "Save"
+
+		within("div.flash") do
+		expect(page).to have_content("There was a problem adding that todo list item.")
+		end
+		expect(page).to have_content("Content is too short")
+	end
 
 end
